235ce8655fef2a3fbd129256d6bc5d85ed8f937b
Pranesh <praneshmeher@gmail.com>
2021-07-02 16:26:28 +0530

    commit

---
+++ creational/constructor-class.js
@@ -0,0 +1,10 @@
+
+class Person{
+    constructor(fname,lname){
+        this.firstname=fname
+        this.lastname=lname
+    }
+}
+
+let person1 = new Person('Pranesh', 'Meher')
+console.log(person1)
