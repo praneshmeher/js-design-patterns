31ec4e6508cebf4d36c3678ace210930eaa7d06b
Pranesh <praneshmeher@gmail.com>
2021-07-02 16:30:01 +0530

    commit

--- creational/constructor-class.js
+++ creational/constructor-class.js
@@ -6,5 +6,15 @@ class Person{
     }
 }
 
+class Student extends Person{
+    constructor(fname,lname,rollno){
+        super(fname,lname)
+        this.rollno=rollno
+    }
+}
+
 let person1 = new Person('Pranesh', 'Meher')
 console.log(person1)
+
+let student1 = new Student('Pranesh','Meher',07)
+console.log(student1)
